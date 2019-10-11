df2e63e59dcb29f0e244af9e9b9bf0a0b49d606f
nazmulbasher <nazmul.basher@fieldnation.com>
2019-10-11 21:37:13 +0600

    Added codes

--- collector/script.js
+++ collector/script.js
@@ -1,3 +1,14 @@
 (function(){
-    console.log("Hello Nazmul");
+    console.log(navigator.userAgent);
+    var url = "http://localhost:8082/push";
+
+    var xhr = new XMLHttpRequest();
+    var json = JSON.stringify({
+        userAgent: navigator.userAgent
+    });
+
+    xhr.open("POST", url)
+    xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
+
+    xhr.send(json);
 })();
