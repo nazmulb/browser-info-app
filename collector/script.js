b709c750025514ec54d408f1de5116671d67eb99
nazmulbasher <nazmul.basher@fieldnation.com>
2019-10-12 11:05:01 +0600

    Added codes

--- collector/script.js
+++ collector/script.js
@@ -1,14 +1,35 @@
-(function(){
-    console.log(navigator.userAgent);
-    var url = "http://localhost:8082/push";
+(function(pushUrl){
+    var Collector = function() {
+        this.getUserAgent = function() {
+            return navigator.userAgent;
+        };
 
-    var xhr = new XMLHttpRequest();
-    var json = JSON.stringify({
-        userAgent: navigator.userAgent
+        this.getPublicIP = function() {
+            return "162.158.62.75";
+        }
+
+        this.getLocalIP = function() {
+            return "192.168.0.10";
+        }
+    }
+
+    var request = function(url, method, data, contentType) {
+        var xhr = new XMLHttpRequest();
+        var cType = "application/json; charset=utf-8";
+        if(contentType) cType = contentType;
+        xhr.open(method, url);
+        xhr.setRequestHeader("Content-type", cType);
+        xhr.send(data);
+    }
+
+    var c1 = new Collector();
+
+    var data = JSON.stringify({
+        userAgent: c1.getUserAgent(),
+        publicIP: c1.getPublicIP(),
+        localIP: c1.getLocalIP()
     });
 
-    xhr.open("POST", url)
-    xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
+    request(pushUrl, "POST", "name=Nazmul&age=38", "application/x-www-form-urlencoded");
 
-    xhr.send(json);
-})();
+})("http://localhost:8082/api/push");
