7b4a55c5cf78e592d52d105eb5a7a28c71279853
nazmulbasher <nazmul.basher@fieldnation.com>
2019-10-12 19:00:49 +0600

    Added codes

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(8082);
+	const app = await NestFactory.create(AppModule);
+	await app.listen(8082);
 }
+
 bootstrap();
