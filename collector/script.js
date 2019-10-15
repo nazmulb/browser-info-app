28c23db48f49926ecfbb3ebc7bd369161322b3d9
nazmulbasher <nazmul.basher@fieldnation.com>
2019-10-15 13:12:30 +0600

    Added codes

--- collector/script.js
+++ collector/script.js
@@ -4,16 +4,58 @@
             return navigator.userAgent;
         };
 
-        this.getPublicIP = function() {
-            return "162.158.62.75";
+        this.getPublicIP = function(callback) {
+            request("http://api.ipify.org", "GET", null, callback);
         }
 
-        this.getLocalIP = function() {
-            return "192.168.0.5";
+        this.getLocalIP = function(callback) {
+            var RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+        
+            if (!RTCPeerConnection) {
+              return 'Your browser does not support this API';
+            }
+            
+            var rtc = new RTCPeerConnection({iceServers:[]});
+            var addrs = {};
+            addrs["0.0.0.0"] = false;
+            
+            function grepSDP(sdp) {
+                var hosts = [];
+                var finalIP = '';
+                sdp.split('\r\n').forEach(function (line) {
+                    if (~line.indexOf("a=candidate")) {
+                        var parts = line.split(' '),
+                            addr = parts[4],
+                            type = parts[7];
+                        if (type === 'host') {
+                            finalIP = addr;
+                        }
+                    } else if (~line.indexOf("c=")) {
+                        var parts = line.split(' '),
+                            addr = parts[2];
+                        finalIP = addr;
+                    }
+                });
+                return finalIP;
+            }
+            
+            if (1 || window.mozRTCPeerConnection) {
+                rtc.createDataChannel('', {reliable:false});
+            };
+            
+            rtc.onicecandidate = function (evt) {
+                if (evt.candidate) {
+                  var addr = grepSDP("a="+evt.candidate.candidate);
+                  callback(addr);
+                }
+            };
+            rtc.createOffer(function (offerDesc) {
+                rtc.setLocalDescription(offerDesc);
+            }, function (e) { console.warn("offer failed", e); });
         }
     }
 
-    var request = function(url, method, data) {
+    var request = function(url, method, data, callback) {
         try {
             var xhr = new XMLHttpRequest();
             xhr.open(method, url, true);
@@ -24,7 +66,10 @@
         }
         
         xhr.onload = function() {
-            console.log("Loaded: " + xhr.status + " " + xhr.response);
+            if (xhr.readyState === xhr.DONE) {
+                console.log("Loaded: " + xhr.status + " " + xhr.response);
+                if(callback) callback(xhr.status, xhr.response);
+            }
         };
 
         xhr.onerror = function() {
@@ -34,13 +79,41 @@
     
     var pushUrl = "http://localhost:8082/push";
     var collector = new Collector();
+    var userAgent = collector.getUserAgent();
+    var isChrome = userAgent.indexOf("Chrome") !== -1;
+    var isFirefox = userAgent.indexOf("Firefox") !== -1;
+    var ipAddresses;
+    var i = 0;
 
-    var data = JSON.stringify({
-        userAgent: collector.getUserAgent(),
-        ipAddresses: collector.getPublicIP() + "," + collector.getLocalIP()
-    });
-    
-    request(pushUrl, "POST", data);
+    if(isChrome || isFirefox) {
+        collector.getLocalIP(function(localIP) {
+            collector.getPublicIP(function(status, response) {
+                i = i + 1;
+
+                if(localIP) ipAddresses = localIP;
+                if(status === 200 && localIP) ipAddresses += "," + response;
+                if(status === 200 && !localIP) ipAddresses = response;
+                
+                var data = JSON.stringify({
+                    userAgent: userAgent,
+                    ipAddresses: ipAddresses
+                });
+                
+                if(i === 1) request(pushUrl, "POST", data);
+            });
+        });
+    } else {
+        collector.getPublicIP(function(status, response) {
+            if(status === 200) ipAddresses = response;
+            
+            var data = JSON.stringify({
+                userAgent: userAgent,
+                ipAddresses: ipAddresses
+            });
+            
+            request(pushUrl, "POST", data);
+        });
+    }
     
     /*
     window.addEventListener("beforeunload", function(e) {
