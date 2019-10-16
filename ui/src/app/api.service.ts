dd4068865d0917889d2de0893178dc20cbb5dd66
nazmulbasher <nazmul.basher@fieldnation.com>
2019-10-16 18:48:26 +0600

    Added codes

---
+++ ui/src/app/api.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private REST_API_SERVER = 'http://localhost:8082';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public stat() {
+    return this.httpClient.get(`${this.REST_API_SERVER}/stat`);
+  }
+
+  public auth() {
+    return this.httpClient.post(`${this.REST_API_SERVER}/auth`, { password: '123' });
+  }
+}
---
+++ ui/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ApiService } from '../api.service';
+
+export interface IStat {
+	insertedTime: string;
+	entriesByTypeOSVersion: any[];
+}
+
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  stats = [];
+  time = '';
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.apiService.stat().subscribe((data: IStat) => {
+      console.log(data);
+
+      this.time = (data) ? new Date(data.insertedTime).toLocaleString() : '';
+      this.stats = (data) ? data.entriesByTypeOSVersion : [];
+    });
+  }
+}
