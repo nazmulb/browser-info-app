b047bf690a0a28e15e1d9a7dd679327fd9132690
nazmulbasher <nazmul.basher@fieldnation.com>
2019-10-18 12:23:03 +0600

    Added codes

--- api/test/browser-info.e2e-spec.ts
+++ api/test/browser-info.e2e-spec.ts
@@ -10,7 +10,7 @@ describe("Browser Info (e2e)", () => {
     });
 
     describe("/push (POST)", () => {
-        test("Sendig data from Chrome with local IP and lang", async () => {
+        test("Sendig data from Mac/Chrome with local IP and lang", async () => {
             const response = await request(app.getHttpServer()).post("/push").send({
                 userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36",
                 ipAddresses: "192.168.0.100",
@@ -29,6 +29,79 @@ describe("Browser Info (e2e)", () => {
 
             expect(response.body).toHaveProperty("id");
             expect(typeof response.body.id).toBe("number");
+
+            expect(response.body).toHaveProperty("createdAt");
+            expect(typeof response.body.createdAt).toBe("string");
+        });
+
+        test("Sendig data from Linux/Firefox with local IP and lang", async () => {
+            const response = await request(app.getHttpServer()).post("/push").send({
+                userAgent: "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0",
+                ipAddresses: "192.168.0.100",
+                acceptLanguage: "en-US",
+            });
+
+            expect(response.status).toBe(201);
+            expect(typeof response.body).toBe("object");
+
+            expect(response.body.userAgent).toBe("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0");
+            expect(response.body.browserType).toBe("Firefox");
+            expect(response.body.osType).toBe("Linux");
+            expect(response.body.browserVersion).toBe("69");
+            expect(response.body.acceptLanguage).toBe("en-US");
+            expect(response.body.ipAddresses).toContain("192.168.0.100");
+
+            expect(response.body).toHaveProperty("id");
+            expect(typeof response.body.id).toBe("number");
+
+            expect(response.body).toHaveProperty("createdAt");
+            expect(typeof response.body.createdAt).toBe("string");
+        });
+
+        test("Sendig data from Windows/IE with lang", async () => {
+            const response = await request(app.getHttpServer()).post("/push").send({
+                userAgent: "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)",
+                acceptLanguage: "en-US",
+            });
+
+            expect(response.status).toBe(201);
+            expect(typeof response.body).toBe("object");
+
+            expect(response.body.userAgent).toBe("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)");
+            expect(response.body.browserType).toBe("IE");
+            expect(response.body.osType).toBe("Windows");
+            expect(response.body.browserVersion).toBe("9");
+            expect(response.body.acceptLanguage).toBe("en-US");
+
+            expect(response.body).toHaveProperty("id");
+            expect(typeof response.body.id).toBe("number");
+
+            expect(response.body).toHaveProperty("ipAddresses");
+            expect(typeof response.body.ipAddresses).toBe("string");
+
+            expect(response.body).toHaveProperty("createdAt");
+            expect(typeof response.body.createdAt).toBe("string");
+        });
+
+        test("Sendig data from Postman", async () => {
+            const response = await request(app.getHttpServer()).post("/push").send({
+                userAgent: "PostmanRuntime/7.1.1",
+            });
+
+            expect(response.status).toBe(201);
+            expect(typeof response.body).toBe("object");
+
+            expect(response.body.userAgent).toBe("PostmanRuntime/7.1.1");
+            expect(response.body.browserType).toBe("Unknown");
+            expect(response.body.osType).toBe("Unknown");
+            expect(response.body.browserVersion).toBeNull();
+            expect(response.body.acceptLanguage).toBeNull();
+
+            expect(response.body).toHaveProperty("id");
+            expect(typeof response.body.id).toBe("number");
+
+            expect(response.body).toHaveProperty("ipAddresses");
+            expect(typeof response.body.ipAddresses).toBe("string");
 
             expect(response.body).toHaveProperty("createdAt");
             expect(typeof response.body.createdAt).toBe("string");
